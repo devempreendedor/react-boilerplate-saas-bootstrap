91e1e421522e04edd62c69e5db559aeb1e4e34c9
Lucas Couto <lucas@orbithub.com.br>
2022-04-01 11:32:14 -0300

    feat: created button

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './styles/main.scss'
+import { Button } from 'react-bootstrap'
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1>React Boilerplate SaaS and Bootstrap</h1>
+      <Button variant="primary">Entrar</Button>
     </div>
   )
 }
